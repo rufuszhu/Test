c081d8335fe6e102ab0adf003b9e345d2f52d9ff
Rufus Zhu <Rufus.Zhu@digital-dispatch.com>
2014-10-08 14:19:18 -0700

    improve about page UI and profile page UI

--- TaxiLimoNewUI/src/digital/dispatch/TaxiLimoNewUI/Drawers/ProfileActivity.java
+++ TaxiLimoNewUI/src/digital/dispatch/TaxiLimoNewUI/Drawers/ProfileActivity.java
@@ -10,10 +10,13 @@ import digital.dispatch.TaxiLimoNewUI.R;
 import digital.dispatch.TaxiLimoNewUI.Utils.Logger;
 import digital.dispatch.TaxiLimoNewUI.Utils.MBDefinition;
 import digital.dispatch.TaxiLimoNewUI.Utils.SharedPreferencesManager;
+import digital.dispatch.TaxiLimoNewUI.Utils.Utils;
 import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
 //import android.text.TextWatcher;
 //import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -24,58 +27,49 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
-
 public class ProfileActivity extends ActionBarActivity {
-	
-	private EditText edtPhone, edtUName, edtUEmail;
+
+	private EditText edtPhone, et_firstName, et_lastName, edtUEmail;
 	private LinearLayout save_btn;
 	private Context _activity;
 	private MenuItem edit_icon;
 	private final String TAG = "ProfileActivity";
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_profile);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		edtPhone = (EditText)findViewById(R.id.edt_phoneNum);
-        edtUName = (EditText)findViewById(R.id.edt_userName);
-        edtUEmail = (EditText)findViewById(R.id.edt_userEmail);
-        save_btn = (LinearLayout) findViewById(R.id.profile_save_btn);
+		edtPhone = (EditText) findViewById(R.id.edt_phoneNum);
+		et_firstName = (EditText) findViewById(R.id.et_firstName);
+		et_lastName = (EditText) findViewById(R.id.et_lastName);
+		edtUEmail = (EditText) findViewById(R.id.edt_userEmail);
+		save_btn = (LinearLayout) findViewById(R.id.profile_save_btn);
 		_activity = this;
-		//name, Phone number and email is not editable at this point
-		edtUName.setFocusable(false);
-		edtUName.setTextColor(getResources().getColor(R.color.gray_light));
-		
+		// name, Phone number and email is not editable at this point
+		et_firstName.setFocusable(false);
+		et_lastName.setFocusable(false);
 		edtPhone.setFocusable(false);
-		edtPhone.setTextColor(getResources().getColor(R.color.gray_light));
-		
 		edtUEmail.setFocusable(false);
-		edtUEmail.setTextColor(getResources().getColor(R.color.gray_light));
-		
-		
+
 		save_btn.setOnClickListener(new View.OnClickListener() {
-		
+
 			public void onClick(View v) {
-				
-				if(validateName()){
-					//update name
+
+				if (validateName()) {
+					// update name
 					storeInfo();
-					
 				}
-				
 			}
-			
 		});
-		
-	
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.profile, menu);
-		//edit_icon = menu.findItem(R.id.action_edit);
+		// edit_icon = menu.findItem(R.id.action_edit);
 		return true;
 	}
 
@@ -87,104 +81,73 @@ public class ProfileActivity extends ActionBarActivity {
 		int id = item.getItemId();
 		if (id == R.id.action_edit) {
 			Logger.d(TAG, "onOptionsItemSelected");
-			edtUName.setFocusable(true);
-			edtUName.setFocusableInTouchMode(true);
+			et_firstName.setFocusable(true);
+			et_firstName.setFocusableInTouchMode(true);
+			et_firstName.setBackground(getResources().getDrawable(R.drawable.shape_textview_border));
 			
-			edtUName.setTextColor(getResources().getColor(R.color.black));
+			et_lastName.setFocusable(true);
+			et_lastName.setFocusableInTouchMode(true);
+			et_lastName.setBackground(getResources().getDrawable(R.drawable.shape_textview_border));
+			save_btn.setVisibility(View.VISIBLE);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-		
-		
+
 	}
-	
-	
+
 	@Override
-    public void onResume() { 
-		
-	     
-	
-	    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(_activity);
-	    if(sharedPreferences != null){
+	public void onResume() {
+		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(_activity);
+		if (sharedPreferences != null) {
 			String phone = SharedPreferencesManager.loadStringPreferences(sharedPreferences, MBDefinition.SHARE_PHONE_NUMBER);
-			
+
 			edtPhone.setText(phone);
-			
+
 			String firstName = SharedPreferencesManager.loadStringPreferences(sharedPreferences, MBDefinition.SHARE_FIRST_NAME);
 			String lastName = SharedPreferencesManager.loadStringPreferences(sharedPreferences, MBDefinition.SHARE_LAST_NAME);
-	
-			
-			edtUName.setText(firstName + " " + lastName);
-			
+
+			et_firstName.setText(firstName);
+			et_lastName.setText(lastName);
 			String email = SharedPreferencesManager.loadStringPreferences(sharedPreferences, MBDefinition.SHARE_EMAIL);
 			edtUEmail.setText(email);
-			
-			
-	    }
-    	super.onResume();
-    }
-    
-	/*
-	private boolean validateEmail() {
-		boolean isValid = true;
-
-		if (edtUEmail.getText().toString().equalsIgnoreCase("")) {
-			edtUEmail.setError(getResources().getString(R.string.profile_empty_email));
-			isValid = false;
-		} else {
-			// check if the entered email is a valid pattern
-			Pattern ePattern = Pattern.compile(MBDefinition.EMAIL_PATTERN);
-			Matcher matcher = ePattern.matcher(edtUEmail.getText().toString());
-
-			if (matcher.matches()) {
-				edtUEmail.setError(null);
-			} else {
-				edtUEmail.setError(getResources().getString(R.string.profile_email_invalid));
-				isValid = false;
-			}
 		}
-		return isValid;
+		super.onResume();
 	}
-	
-	private boolean validatePhone() {
-		String name = edtPhone.getText().toString();
-		if (name.length() == 0) {
-			edtPhone.setError(getResources().getString(R.string.empty_phone_number));
-			return false;
-		} else if (name.length() > 13) {
-			edtPhone.setError(getResources().getString(R.string.phone_number_too_long));
-			return false;
-		} else
-			return true;
-	}
-	*/
+
 	private boolean validateName() {
-		String name = edtUName.getText().toString();
-		if (name.length() == 0) {
-			edtUName.setError(getResources().getString(R.string.profile_empty_name));
+		String first_name = et_firstName.getText().toString();
+		String last_name = et_lastName.getText().toString();
+		if (first_name.length() == 0) {
+			et_firstName.setError(getResources().getString(R.string.profile_empty_name));
 			return false;
-		} else {
-			return true;
 		}
+		if (last_name.length() == 0) {
+			et_lastName.setError(getResources().getString(R.string.profile_empty_name));
+			return false;
+		}
+
+		return true;
+
 	}
-	
+
 	private void storeInfo() {
 		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences((_activity));
-		String name = edtUName.getText().toString();
-		
-		String[] names = name.split(" ");
-		//assume that last name is the name after the last white space and the rest of it are first name
-		String lastName = names[names.length-1];
-		
-		String firstName = name.substring(0, (name.length() - lastName.length() -1));
-		
+		String firstName = et_firstName.getText().toString();
+		String lastName = et_lastName.getText().toString();
+
 		SharedPreferencesManager.savePreferences(sharedPreferences, MBDefinition.SHARE_FIRST_NAME, firstName);
 		SharedPreferencesManager.savePreferences(sharedPreferences, MBDefinition.SHARE_LAST_NAME, lastName);
-		//SharedPreferencesManager.savePreferences(sharedPreferences, MBDefinition.SHARE_EMAIL, edtUEmail.getText().toString());
-		//SharedPreferencesManager.savePreferences(sharedPreferences, MBDefinition.SHARE_PHONE_NUMBER, edtPhone.getText().toString());
-		edtUName.setFocusable(false);
-		edtUName.setTextColor(getResources().getColor(R.color.gray_light));
-	
+		// SharedPreferencesManager.savePreferences(sharedPreferences, MBDefinition.SHARE_EMAIL, edtUEmail.getText().toString());
+		// SharedPreferencesManager.savePreferences(sharedPreferences, MBDefinition.SHARE_PHONE_NUMBER, edtPhone.getText().toString());
+		AlertDialog.Builder builder = new AlertDialog.Builder(_activity);
+		builder.setMessage(R.string.save_successful).setNegativeButton(R.string.ok, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				dialog.dismiss();
+				finish();
+			}
+		});
+
+		builder.show();
 	}
-	
+
 }
